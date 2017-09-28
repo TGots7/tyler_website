1340b178a106c7bf0b0d4a01f4c82209bc73c518
TGots7 <tylergotto17@gmail.com>
2017-09-27 23:18:45 -0400

    box shadow

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -5,6 +5,11 @@ class UserMailer < ApplicationMailer
 	    @user = user
 	    # @url  = "http://example.com/login"
 	    mail(:to => @user.email, :subject => "Welcome to My Awesome Site")
+
+	    # options = {
+	    # 	to: @user.email,
+	    # 	subject: "Weclome to Tyler Gotto's Website"
+	    # }
 	  end
 
 	
