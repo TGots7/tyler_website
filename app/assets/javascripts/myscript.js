16b495a2a7ffb440adc6ee69337540917ef22841
TGots7 <tylergotto17@gmail.com>
2017-10-09 22:47:16 -0400

    cosmetics

---
+++ app/assets/javascripts/myscript.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+      $('.wish-list').click(function(){
+        location.href = 'https://twitter.com/TGots7?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor'
+  })
+});
+
+
+
