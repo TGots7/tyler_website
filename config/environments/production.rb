657b9e1ea2fe8470634920ecbaa5d94fe15154ae
TGots7 <tylergotto17@gmail.com>
2017-11-06 14:51:23 -0500

    mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,10 +64,10 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
    :port                 => 587,
-   :user_name            => ENV['gmail_username'],
-   :password             => ENV['gmail_password'],
+   :user_name            => ENV['tylergotto17@gmail.com'],
+   :password             => ENV['canucks77'],
    :authentication       => "plain",
-  :enable_starttls_auto => true
+   :enable_starttls_auto => true
   }
 
   # Ignore bad email addresses and do not raise email delivery errors.
