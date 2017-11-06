aa0eb5d0121e405d54aadb00df8d805d6fe33965
TGots7 <tylergotto17@gmail.com>
2017-11-06 13:29:30 -0500

    testing removed javascript

--- app/assets/javascripts/myscript.js
+++ app/assets/javascripts/myscript.js
@@ -9,30 +9,30 @@ $(document).ready(function(){
     $('.storypage').click(function(){
       window.open('http://www.gottomarrygrace.com/story.php', "_blank");
   })
-    $('.email-button').on("click", function(e){
+  //   $('.email-button').on("click", function(e){
       
-      url = "/users"
-      data = {
-        'authenticity_token': $("input[name='authenticity_token']").val(),
-        'user': {'name': $('#user_name').val(),
-        'email': $('#user_email').val()
-        }
-      };
-      $.ajax({
-        type: "POST",
-        url: url,
-        data: data,
-        success: function(response){
-          $('#user_name').val("")
-          $('#user_email').val("")
-          $('#call_to_action').hide()
-          var $cta = $('#call_to_action2')
-          $cta.html(response);
-        }
-      })
+  //     url = "/users"
+  //     data = {
+  //       'authenticity_token': $("input[name='authenticity_token']").val(),
+  //       'user': {'name': $('#user_name').val(),
+  //       'email': $('#user_email').val()
+  //       }
+  //     };
+  //     $.ajax({
+  //       type: "POST",
+  //       url: url,
+  //       data: data,
+  //       success: function(response){
+  //         $('#user_name').val("")
+  //         $('#user_email').val("")
+  //         $('#call_to_action').hide()
+  //         var $cta = $('#call_to_action2')
+  //         $cta.html(response);
+  //       }
+  //     })
       
-      e.preventDefault();
-  })
+  //     e.preventDefault();
+  // })
 });
 
 
