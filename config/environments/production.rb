0006de65a897c2eeb922d0788e26a7760cdfed81
TGots7 <tylergotto17@gmail.com>
2017-11-06 14:57:54 -0500

    email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,6 +60,8 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "tyler_#{Rails.env}"
+  config.action_mailer.default_url_options = { :host => 'www.tylergotto.com' }
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_caching = false
   config.action_mailer.smtp_settings = {
    :address              => "smtp.gmail.com",
