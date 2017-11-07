73ac23ab59219f82c0fac5681541a7d13b7530d7
TGots7 <tylergotto17@gmail.com>
2017-11-07 15:53:05 -0500

    added mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,10 @@
 class UserMailer < ApplicationMailer
 	default :from => "Tyler Gotto"
 
-	def welcome_email(user)
+	def welcome_email(nameOF, email)
 	    @user = user
 	    # @url  = "http://example.com/login"
-	    mail(:to => @user.email, :subject => "Welcome to My Awesome Site")
+	    mail(:to => "tylergotto17@gmail.com", :subject => "Tyler, #{nameOF}, from #{email} got an email")
 	 end
 
 	 def mobile_email(nameOF, email)
